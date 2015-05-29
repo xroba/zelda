d422ec8eb44c5785b9575b71fbb24c2fcf888892
Xavier <xavier.idweaver.com>
2015-05-29 18:05:03 +0200

    all

---
+++ Assets/Scripts/Objects/Attackable/AttackableBase.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackableBase : MonoBehaviour {
+	
+	public virtual void onHit(ItemType itemType){
+		Debug.Log ("Non onHit Method for " + gameObject.name);
+	}
+}
